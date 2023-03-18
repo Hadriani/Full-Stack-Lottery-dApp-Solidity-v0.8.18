215e83d71fd203c59cbe4bdb761311cc58e00a0c
Adrian Păun <79643678+Hadriani@users.noreply.github.com>
2023-03-18 17:20:51 +0200

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,18 @@
+import Header from '../components/Header'
+import PotCard from '../components/PotCard'
+import Table from '../components/Table'
+
+import style from '../styles/Home.module.css'
+
+export default function Home() {
+
+  return (
+
+    <div className={style.wrapper}>
+      <Header />
+      <PotCard />
+      <Table />
+    </div>
+
+  )
+};
