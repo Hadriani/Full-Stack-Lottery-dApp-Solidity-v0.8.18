4a1b6f1fe26f0acee3ab08e0d09e83a261b940fe
Adrian Păun <79643678+Hadriani@users.noreply.github.com>
2023-03-25 22:23:26 +0200

    Add files via upload

--- components/Header.js
+++ components/Header.js
@@ -4,15 +4,13 @@ import style from '../styles/Header.module.css'
 import UserCard from './UserCard'
 import WalletConnectBtn from '../components/ConnectWalletBtn'
 
+
 const Header = () => {
   const { address, connectWallet } = useAppContext();
 
   return (
     <div className={style.wrapper}>
       <div className={style.title}>Lottery dApp 💰</div>
-     
-        
-   
       {!address ? (
         <WalletConnectBtn connectWallet={connectWallet} />
       ) : (
