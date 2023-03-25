4875eca37c0f75933bb68688f41ddcb865169b14
Adrian Păun <79643678+Hadriani@users.noreply.github.com>
2023-03-25 21:19:12 +0200

    updated with verify contract installed

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,6 +3,8 @@
 */
 require('dotenv').config();
 require("@nomiclabs/hardhat-ethers");
+require("@nomiclabs/hardhat-etherscan");
+
 const { API_URL, PRIVATE_KEY } = process.env;
 module.exports = {
   solidity: "0.8.18",
@@ -12,6 +14,9 @@ module.exports = {
     sepolia: {
       url: API_URL,
       accounts: [`0x${PRIVATE_KEY}`]
-    }
+    },
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_KEY,
   },
 }
