51a9d6d22c02f69c82eba31a829c18d08faf7a35
Adrian Păun <79643678+Hadriani@users.noreply.github.com>
2023-03-25 22:24:07 +0200

    Add files via upload

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,10 @@
 import '../styles/globals.css'
 import { AppProvider } from '../context/context'
-import AudioPlayer from '../components/AudioPlayer';
+
 
 function MyApp({ Component, pageProps }) {
   return (
     <AppProvider>
-        <AudioPlayer />
         <Component {...pageProps} />
     </AppProvider>
   )
