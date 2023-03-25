6ae78dfb08124d80747ced842cb215033f4a7eed
Adrian Păun <79643678+Hadriani@users.noreply.github.com>
2023-03-25 21:15:59 +0200

    Add files via upload
    updated audio player and other

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,12 @@
 import '../styles/globals.css'
 import { AppProvider } from '../context/context'
+import AudioPlayer from '../components/AudioPlayer';
 
 function MyApp({ Component, pageProps }) {
   return (
     <AppProvider>
-      <Component {...pageProps} />
+        <AudioPlayer />
+        <Component {...pageProps} />
     </AppProvider>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
+import React from 'react';
 import Header from '../components/Header'
 import PotCard from '../components/PotCard'
 import Table from '../components/Table'
+import Link from 'next/link';
 
 import style from '../styles/Home.module.css'
 
@@ -10,6 +12,15 @@ export default function Home() {
 
     <div className={style.wrapper}>
       <Header />
+      <div>
+        <ul className={style.headblocks}>
+          <il className={style.about}>
+            <Link href="/about">Instructions</Link>
+          </il>
+          <il className={style.leaderboard}>Leaderboard</il>
+          <il className={style.us}>Contact Us</il>
+        </ul>
+      </div>
       <PotCard />
       <Table />
     </div>
